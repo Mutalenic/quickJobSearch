fc0818148633126a8075287a65f9a490a3369ae3
Nicholas Mutale <nicomutale@gmail.com>
2023-04-25 16:17:06 +0000

    Add constants

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const Home = () => {
     return (
         <View>
-            <Text>Hello world</Text>
+            <Text>Hi this is my new application</Text>
         </View>
     );
 }

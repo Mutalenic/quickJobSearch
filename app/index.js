5ecd74b9160a253c8710cafd1e8b2ce9f4bad192
Nicholas Mutale <nicomutale@gmail.com>
2023-04-26 17:12:33 +0000

    Define router

--- app/index.js
+++ app/index.js
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
 
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>Hi this is my new application</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor:COLORS.lightwhite }}>
+            <Stack.Screen
+            options= {{
+                headerStyle:{ backgroundColor:COLORS.lightwhite}
+            }}
+            />
+        </SafeAreaView>
     );
 }
 

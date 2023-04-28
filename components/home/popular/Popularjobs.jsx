8db19cdfce238efc1c9552995c9172771711ed99
Nicholas Mutale <nicomutale@gmail.com>
2023-04-28 16:59:37 +0000

    Add imports define router

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useState } from 'react'
+import { useRouter } from 'expo-router'
+import { COLORS,SIZES } from '../../../constants'
 import styles from './popularjobs.style'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+
+const router = useRouter();
 
 const Popularjobs = () => {
   return (

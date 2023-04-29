adec512e6b64e96a5244db5c3b69bcb8cdf96748
Nicholas Mutale <nicomutale@gmail.com>
2023-04-29 08:04:13 +0000

    Update Popularjobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
-import { useState } from 'react'
-import { useRouter } from 'expo-router'
-import { COLORS,SIZES } from '../../../constants'
-import styles from './popularjobs.style'
-import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import { COLORS, SIZES } from "../../../constants";
+import styles from "./popularjobs.style";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
-const router = useRouter();
 
 const Popularjobs = () => {
+const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Popular Jobs</Text>
+      </View>
+      <View style={styles.content}>
+        <FlatList
+          data={Popularjobs}
+          renderItem={({ item }) => <PopularJobCard job={item} />}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;

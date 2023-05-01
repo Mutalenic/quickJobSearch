bc7200944522543ef134988d29dcc94b3281f1a5
Nicholas Mutale <nicomutale@gmail.com>
2023-05-01 20:44:24 +0000

    fetchdata

--- hook/UseFetch.js
+++ hook/UseFetch.js
@@ -5,7 +5,7 @@ import { RAPID_API_KEY} from '@env';
 const useFetch = (endpoint,query) => {{
     const[data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, seterror] = useState(null);
+    const [error, setError] = useState(null);
     const rapidApiKey = RAPID_API_KEY
 
     const axios = require('axios');
@@ -27,6 +27,26 @@ try {
 	console.log(response.data);
 } catch (error) {
 	console.error(error);
+}
+
+const fetchData = async ()=>{
+  setIsLoading(true)
+  try {
+    const response = await axios.request
+    (options)
+    setData(response.data.data);
+    setIsLoading(false);
+  }
+  catch(error) {
+    setError(error);
+    alert('There is an error')
+  }
+  finally {
+    setIsLoading(false);
+  }
+  useEffect(()=>{
+    fetchData();
+  },[])
 }
 
 

59d76d62d60e228cb2507fee6fba3593e331d43a
Nicholas Mutale <nicomutale@gmail.com>
2023-05-01 20:33:51 +0000

    Pass dynamic query add query in params

--- hook/UseFetch.js
+++ hook/UseFetch.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { RAPID_API_KEY} from '@env';
 
-const useFetch = (endpoint) => {{
+const useFetch = (endpoint,query) => {{
     const[data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, seterror] = useState(null);
+    const rapidApiKey = RAPID_API_KEY
 
     const axios = require('axios');
 
@@ -12,12 +14,10 @@ const options = {
   method: 'GET',
   url: 'https://jsearch.p.rapidapi.com/${endpoint}',
   params: {
-    query: 'Python developer in Texas, USA',
-    page: '1',
-    num_pages: '1'
+    ...query
   },
   headers: {
-    'X-RapidAPI-Key': ,
+    'X-RapidAPI-Key':rapidApiKey,
     'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
   }
 };

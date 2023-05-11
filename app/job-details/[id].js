e6bbac5744d7566d1170deb468d459e261d13729
Nicholas Mutale <nicomutale@gmail.com>
2023-05-11 15:49:09 +0000

    Add light white background remove header shadow

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,25 @@
-import {Text} from "react-native";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import {SafeAreaView, Text} from "react-native";
+import useFetch from '../../hook/UseFetch';
+import { COLORS, icons, SIZES } from "../../constants";
+
 
 const  JobDetails = ()=> {
+    const params = useSearchParams();
+    const router = useRouter();
+    const { data, isLoading, error, refetch} = useFetch('job-details',{
+        job_id: params.id
+    })
     return (
-        <Text>Job details</Text>
+        <SafeAreaView style={{ flex:1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+            options={{
+                headerStyle: {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible: false,
+                
+            }}/>
+
+        </SafeAreaView>
     )
 }
 

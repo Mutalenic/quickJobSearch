9d0e847965ea4a586ed195e679be64756258ed18
Nicholas Mutale <nicomutale@gmail.com>
2023-05-11 16:39:07 +0000

    update dynamic route

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -18,16 +18,25 @@ const  JobDetails = ()=> {
         job_id: params.id
     })
     return (
-        <SafeAreaView style={{ flex:1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen
-            options={{
-                headerStyle: {backgroundColor: COLORS.lightWhite},
-                headerShadowVisible: false,
-                headerBackVisible: false,
-                headerLeft : () =>( <ScreenHeaderBtn iconUrl={icons.left} dimensions = "60%"handlePress={() => router.back()}/>),
-                headerTitle:''
-
-            }}/>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <Stack.Screen
+          options={{
+            headerStyle: { backgroundColor: COLORS.lightWhite },
+            headerShadowVisible: false,
+            headerBackVisible: false,
+            headerLeft: () => (
+              <ScreenHeaderBtn
+                iconUrl={icons.left}
+                dimension='60%'
+                handlePress={() => router.back()}
+              />
+            ),
+            headerRight: () => (
+              <ScreenHeaderBtn iconUrl={icons.share} dimension='60%' />
+            ),
+            headerTitle: ""
+          }}
+        />
 
         </SafeAreaView>
     )
